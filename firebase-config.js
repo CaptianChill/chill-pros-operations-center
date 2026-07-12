c01340e429eef056aa4d73f3378da12aa6333209
CaptianChill <chillprostx@gmail.com>
2026-07-12 16:44:23 -0500

    Add Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsBEKMggwSUvEmdTTK1rjYOcdPyYCCLOc",
+  authDomain: "chill-pros-ice-stream.firebaseapp.com",
+  projectId: "chill-pros-ice-stream",
+  storageBucket: "chill-pros-ice-stream.firebasestorage.app",
+  messagingSenderId: "260000821827",
+  appId: "1:260000821827:web:b9ace31bc5c1093deedaf6",
+  measurementId: "G-V2TJE0RWPR"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+window.chillProsDb = db;
