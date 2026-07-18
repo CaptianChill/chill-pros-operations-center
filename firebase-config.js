d53254edcf770af32f085006247f907acd316129
CaptianChill <chillprostx@gmail.com>
2026-07-18 15:19:19 -0500

    Sync Firebase web configuration

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBsBEKMggwSUVEmdTTK1rjYOcdPyYCCL0c",
   authDomain: "chill-pros-ice-stream.firebaseapp.com",
   projectId: "chill-pros-ice-stream",
-  storageBucket: "chill-pros-ice-stream.appspot.com",
+  storageBucket: "chill-pros-ice-stream.firebasestorage.app",
   messagingSenderId: "260000821827",
-  appId: "1:260000821827:web:b9ace31bc5c1093deedaf6",
-  measurementId: "G-V2TJE0RWPR"
+  appId: "1:260000821827:web:4d65bb9f17a29001eedaf6",
+  measurementId: "G-R0T9MTVV1V"
 };
 
 firebase.initializeApp(firebaseConfig);
