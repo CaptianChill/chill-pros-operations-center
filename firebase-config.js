c7ce5881dc6541d892cc5ac812eea8f89e87c473
CaptianChill <chillprostx@gmail.com>
2026-07-17 19:11:26 -0500

    Update Firebase API key and fix syntax error

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBsBEKMggwSUvEmdTTK1rjYOcdPyYCCLOc",
+  apiKey: "AIzaSyBsBEKMggwSUVEmdTTK1rjYOcdPyYCCL0c",
   authDomain: "chill-pros-ice-stream.firebaseapp.com",
   projectId: "chill-pros-ice-stream",
-  storageBucket: storageBucket: "chill-pros-ice-stream.appspot.com",  messagingSenderId: "260000821827",
+  storageBucket: "chill-pros-ice-stream.appspot.com",
+  messagingSenderId: "260000821827",
   appId: "1:260000821827:web:b9ace31bc5c1093deedaf6",
   measurementId: "G-V2TJE0RWPR"
 };
